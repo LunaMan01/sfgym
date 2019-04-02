28ff01377f6cc751f84debe0325746d721e5bc12
Luna16290847 <luis-luna98@hotmail.com>
2019-04-02 14:53:28 -0600

    AñadirCleinte

--- php/clientes/añadir.php
+++ php/clientes/añadir.php
@@ -2,7 +2,15 @@
     include 'conexion.php'
 
     $nombre_cliente = $_POST['nombre_cliente']
-    $ap_parno = $_POST['ap-parno']
+    $apellido_paterno = $_POST['ap-parno']
 
-    
+    $sql = "INSERT INTO Clientes (nombre_cliente, apellido_paterno) VALUES ('".$nombre_cliente."', '".$apellido_paterno."')";
+
+    if (mysqli_query($conn, $sql)) {
+        echo "New record created successfully";
+    } else {
+        echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+    }
+
+    $conn = null;
 ?>

7ce414e5e8ea9ac8fe1c16824e42fbb15a3ae949
CarlosVega250 <csvega250@gmail.com>
2019-04-02 18:35:08 -0600

    Envio de datos

--- js/clientes/addCliente.js
+++ js/clientes/addCliente.js
@@ -1,11 +1,8 @@
-
-
-
-
 document.querySelector('#form').addEventListener('submit', function() {
-     var form = document.querySelector('form');
+    var form = document.querySelector('form');
     var data = new FormData(form);
     var req = new XMLHttpRequest();
+    req.open("POST", 'php/clientes/añadir.php', false);
     req.send(data);
     console.log(req.responseText);
 })

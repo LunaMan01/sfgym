b849f3dbfd968a5cf6e84b723c7f7fcc147cff78
CarlosVega250 <csvega250@gmail.com>
2019-04-03 02:26:43 -0600

    modificar clientes

--- js/clientes/editar-cliente.js
+++ js/clientes/editar-cliente.js
@@ -4,13 +4,29 @@ function editClient () {
     var i = event.target;
     var td = i.parentNode;
     var tr = td.parentNode;
-    
     var elements = tr.childNodes;
     var th = elements[1];
     var id = th.getAttribute('id');
-    console.log(id);
+    localStorage.setItem("id", id);
+    req = new XMLHttpRequest();
+    req.open("POST", 'html/clientes-components/editar-cliente.html', false);
+    req.send(null);
+    document.querySelector('.content').innerHTML = req.responseText;
+
+    getDatosCliente(id);
 }
 
 for (var i = 0; i < elements.length; i++) {
     elements[i].addEventListener('click', editClient, false);
+}
+
+function getDatosCliente(id) {
+    var req = new XMLHttpRequest();
+    req.open("POST", 'php/clientes/get-datos-cliente.php', false);
+    req.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
+    var params = 'id='+id 
+    req.send(params);
+    var elements = req.responseText;
+    var cliente = JSON.parse(elements);
+    console.log(cliente);
 }

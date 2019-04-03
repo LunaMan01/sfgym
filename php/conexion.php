8df051f81b5daac6c72944605b018e802a3d9656
CarlosVega250 <csvega250@gmail.com>
2019-04-02 21:45:11 -0600

    Conexion luna

--- php/conexion.php
+++ php/conexion.php
@@ -1,16 +1,15 @@
-<?php
-    $servername = "localhost";
-    $username = "root";
-    $password = "";
 
-    try {
-        $conn = mysqli_connect("$servername","$username","$password");
-        mysqli_select_db($conn, "nuevoacropolisgym");
-        // set the PDO error mode to exception
-        // $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-        echo "Connected successfully";
-    }
-    catch(PDOException $e){
-        echo "Connection failed: " . $e->getMessage();
-    }
+  <?php
+  $servername = "localhost";
+  $username = "root";
+  $password = "";
+  try {
+      $conn = new PDO("mysql:host=$servername;dbname=nuevoacropolisgym", $username, $password);
+      // set the PDO error mode to exception
+      $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+      echo "Connected successfully";
+  }
+  catch(PDOException $e){
+      echo "Connection failed: " . $e->getMessage();
+  }
 ?> 

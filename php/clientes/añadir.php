bc95029e9491fb6771b05b63ba5a4059abaade0f
Luna16290847 <luis-luna98@hotmail.com>
2019-04-03 02:44:24 -0600

    AñadirClientes PHP 1.0

--- php/clientes/añadir.php
+++ php/clientes/añadir.php
@@ -3,22 +3,44 @@
     $conex = $conn;
     try {
         echo "Conexion";
-        $preparar = $conn->prepare('INSERT INTO Clientes (nombre_cliente, apellido_paterno, apellido_materno, edad) 
+
+    //DATOS MAS BASICOS DEL CLIENTE    
+        $cliente = $conn->prepare('INSERT INTO Clientes (nombre_cliente, apellido_paterno, apellido_materno, edad) 
         VALUES (:nombre, :paterno, :materno, :edad)');
         
-        $preparar->bindParam(':nombre', $_POST['nombre_cliente']);
-        $preparar->bindParam(':paterno', $_POST['ap-parno']);
-        $preparar->bindParam(':materno', $_POST['ap-marno']);
-        $preparar->bindParam(':edad', $_POST['edad']);
+        $cliente->bindParam(':nombre', $_POST['nombre_cliente']);
+        $cliente->bindParam(':paterno', $_POST['ap-parno']);
+        $cliente->bindParam(':materno', $_POST['ap-marno']);
+        $cliente->bindParam(':edad', $_POST['edad']);
         //$preparar->bindParam(':activo', $_POST['']);
         //$preparar->bindParam(':genero', $_POST['']);
-        
-        $preparar->execute();
+        $cliente->execute();
+
+    //INSERTAR TELEFONO
+        $lastId = $conn->lastInsertId();
+
+        $telefono = $conn->prepare('INSERT INTO Telefonos (Id_Cliente, numero) 
+        VALUES (:ID, :numero)');
+
+        $telefono->bindParam(':ID', $lastId);
+        $telefono->bindParam(':numero', $_POST['telefono']);
+        $telefono->execute();
+    
+    //INSERTAR DIRECCION
+        $direccion = $conn->prepare('INSERT INTO Direcciones (Id_Cliente, colonia, calle, numero_exterior, numero_interior)
+        VALUES (:ID, :col, :cal, :numeroex, :numeroin)');
+
+        $direccion->bindParam('ID', $lastId);
+        $direccion->bindParam('col', $_POST['colonia']);
+        $direccion->bindParam('cal', $_POST['calle']);
+        $direccion->bindParam('numeroex', $_POST['num-ext']);
+        $direccion->bindParam('numeroin', $_POST['num-int']);
+        $direccion->execute();
+
         echo "New records created successfully";
     }   
     catch(PDOException $e){
         echo "Error: ". $e->getMessage();
-        console_log("Error");
     }
     $conn = null;
 ?>

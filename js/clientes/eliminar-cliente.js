def876d2ac9bd2597e0af74a9faa9d67299c87e9
CarlosVega250 <csvega250@gmail.com>
2019-04-09 13:52:21 -0500

    Boton cancelar y correccion de eliminacion de clientes

--- js/clientes/eliminar-cliente.js
+++ js/clientes/eliminar-cliente.js
@@ -1,29 +1,46 @@
-var elements = document.querySelectorAll('.delete-action');
-var id = 0;
-var tr = null;
-function getIdCliente () {
+// var elementsToBindEvent = document.querySelectorAll('.delete-action');
+// for (var i = 0; i < elementsToBindEvent.length; i++) {
+//     elementsToBindEvent[i].addEventListener('click', getIdCliente);
+// }
+// var id = 0;
+// var tr = null;
+// console.log('1');
+function cargarEventos() {
+    let iElements = document.querySelectorAll('.delete-action');
+    
+    iElements.forEach(i => {
+        i.addEventListener('click', getIdCliente);
+    })
+    
+    console.log('cargar events');
+    return true
+}
+
+cargarEventos();
+
+function getIdCliente() {
     var i = event.target;
     var td = i.parentNode;
     tr = td.parentNode;
+    console.log(tr);
     var elements = tr.childNodes;
     var th = elements[1];
     id = th.getAttribute('id');
-    
+
+
 }
+
 
 function eliminarCliente() {
     var req = new XMLHttpRequest();
     req.open("POST", 'php/clientes/eliminarCliente.php', false);
     req.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
-    req.send('id='+id);
+    req.send('id=' + id);
     tr.remove();
-    new Toast('Cliente eliminado correctamente',2000,'#mensaje', 'success').show();
-   
+    new Toast('Cliente eliminado correctamente', 2000, '#mensaje', 'success').show();
+
 }
 
-for (var i = 0; i < elements.length; i++) {
-    elements[i].addEventListener('click', getIdCliente, false);
-}
 
 document.querySelector('#confirmar-eliminacion').addEventListener('click', eliminarCliente);
-   
+
--- js/components.js
+++ js/components.js
@@ -1,13 +1,17 @@
+const divAdicional = document.querySelector('#elementos-adicionales');
 class Toast {
-    constructor(text, time, ToastName, type) {
+    constructor(text, time, ToastName, type, idTemplate) {
         this.text = text;
         this.time = time;
         this.ToastName = ToastName
         this.type = type;
+        this.idTemplate = idTemplate;
     }
 
     show() {
+        document.querySelector('.modal-ommission').classList.remove('d-none');
         var content = document.createTextNode(this.text);
+    
         document.querySelector('.alert-text').appendChild(content);
         document.querySelector('.modal-div').classList.add('fixed-top');
         if (this.type == 'success') {
@@ -16,12 +20,53 @@ class Toast {
             document.querySelector(this.ToastName).classList.add('alert-danger');
         }
         document.querySelector(this.ToastName).classList.add('show');
+        console.log('una');
         setTimeout(() => {
             document.querySelector(this.ToastName).classList.remove('show');
 
             document.querySelector('.alert-text').innerHTML = '';
+            document.querySelector('.modal-div').classList.remove('fixed-top');
+            document.querySelector('.modal-ommission').classList.add('d-none');
         }, this.time);
 
     }
+    
 }
 
+class Button {
+    constructor(text, id) {
+        this.text = text;
+        this.id = id;
+    }
+
+    getButton() {
+        return `
+            <div class="d-flex btn-add">
+            <button class="btn btn-outline-success btn-sm my-2 my-sm-0 mr-3 rounded-pill" type="button" id="${this.id}">${this.text}</button>
+            </div>
+        `;
+    }
+}
+
+class SearchInput {
+    constructor (id) {
+        this.id = id;
+    }
+
+    getSearchInput() {
+        return `
+        <div class"d-flex" id="buscar-div">
+            <input class="form-control  rounded-pill search" type="search" placeholder="Buscar" aria-label="Search" id="${this.id}">
+        </div>`;
+    }
+}
+
+class Tr {
+    constructor(tr) {
+        this.tr = tr;
+    }
+
+    getTr(){
+        return this.tr;
+    }
+}

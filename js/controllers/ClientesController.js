e1fe086b27f4413e97ca415d4cbbd376c137837c
CarlosVega250 <csvega250@gmail.com>
2019-04-17 01:12:42 -0500

    Busqueda dinamica

--- js/controllers/ClientesController.js
+++ js/controllers/ClientesController.js
@@ -1,19 +1,19 @@
 var clienteController = (function () {
 
-    
+
     function addNuevoCliente() {
         var data = UICliente.getDatosParaNuevoCliente();
         let cliente = new Cliente();
-        
-        if(cliente.add(data)) {
+
+        if (cliente.add(data)) {
             UICliente.mostrarMensajeExito('Cliente añadido correctamente');
         }
-        
+
     }
 
     function eliminarCliente() {
         let cliente = new Cliente();
-        if(cliente.eliminar()) {
+        if (cliente.eliminar()) {
             UICliente.mostrarMensajeExito('Cliente eliminado correctamente');
             UICliente.quitarRegistro();
         }
@@ -23,7 +23,7 @@ var clienteController = (function () {
         let data = UICliente.getDatosModificados();
         let cliente = new Cliente();
         UICliente.mostrarAnimacionBtn('#guardar-cliente-editado');
-        if(cliente.modificar(data)) {
+        if (cliente.modificar(data)) {
             UICliente.mostrarMensajeExito('Cliente modificado correctamente');
             UICliente.regresarBtnAEstadoInicial('#guardar-cliente-editado');
         }
@@ -34,35 +34,55 @@ var clienteController = (function () {
         UICliente.getId(e);
         let cliente = UICliente.getCliente();
         UICliente.setDatosClienteEnInputs(cliente);
-        document.getElementById('form-edit-cliente').addEventListener('submit',modificarCliente);
+        document.getElementById('form-edit-cliente').addEventListener('submit', modificarCliente);
         document.querySelector('#cancelar-cliente').addEventListener('click', UICliente.regresar);
     }
 
-    
 
-    function setUpVentantaNuevoCliente () {
+
+    function setUpVentantaNuevoCliente() {
         UICliente.abrirAddCliente();
         document.querySelector('#form').addEventListener('submit', addNuevoCliente);
-        document.querySelector('#cancelar-cliente').addEventListener('click',UICliente.regresar);
+        document.querySelector('#cancelar-cliente').addEventListener('click', UICliente.regresar);
     }
 
     function setUpDeleteEvent() {
-        document.querySelector('#cuerpo-tabla-clientes').addEventListener('click', function(e) {
-           
-            if(e.target.matches('.delete-action')) {
+        document.querySelector('#cuerpo-tabla-clientes').addEventListener('click', function (e) {
+
+            if (e.target.matches('.delete-action')) {
                 UICliente.getId(e);
             }
-        },false);
-       
+        }, false);
+
     }
 
     function setUpEditEvent() {
-        document.querySelector('#cuerpo-tabla-clientes').addEventListener('click', function(e) {
-           
-            if(e.target.matches('.edit-action')) {
+        document.querySelector('#cuerpo-tabla-clientes').addEventListener('click', function (e) {
+
+            if (e.target.matches('.edit-action')) {
                 setUpVentanaModificarCliente(e);
             }
-        },false);
+        }, false);
+    }
+
+    function setUpWatchEvent() {
+        document.querySelector('#cuerpo-tabla-clientes').addEventListener('click', function (e) {
+
+            if (e.target.matches('.watch-action')) {
+                UICliente.getId(e);
+                let cliente = UICliente.getCliente();
+                UICliente.verCliente(cliente);
+                document.querySelector('#ok-btn').addEventListener('click', UICliente.regresar);
+            }
+        }, false);
+    }
+
+    function busquedaDinamica() {
+        let dato = UICliente.getDatosABuscar();
+        let cliente = new Cliente();
+        let datosEncontrados = cliente.consultar(dato);
+        UICliente.mostrarDatosEncontrados(datosEncontrados);
+        
     }
 
     function setUpEvents() {
@@ -73,13 +93,16 @@ var clienteController = (function () {
         setUpDeleteEvent();
         document.querySelector('#confirmar-eliminacion').addEventListener('click', eliminarCliente);
         setUpEditEvent();
-        
+        setUpWatchEvent();
+        document.querySelector('#buscar-cliente-input').addEventListener('keyup', busquedaDinamica);
     }
+
+  
 
     return {
         init: function () {
-           setUpEvents();
-           
+            setUpEvents();
+
         }
     }
 

84c6181c2b3489fef761dbd6f5c31c6ea55f6e91
Luna16290847 <luis-luna98@hotmail.com>
2019-04-18 20:08:38 -0500

    eliminar, modificar 'mejorado'

--- php/membresias/eliminarMembresias.php
+++ php/membresias/eliminarMembresias.php
@@ -2,8 +2,7 @@
     include '../conexion.php';
 
     try{
-        $delete = "DELETE FROM Membresias WHERE Id_Cliente LIKE ". $_POST['id'].
-        "AND fecha_inicio LIKE ".$_POST['inicio']."AND fecha_fin LIKE ".$_POST['fin'];
+        $delete = "DELETE FROM Membresias WHERE Id_Membresia LIKE ". $_POST['id'];
         
         $conn->execute($delete);
 

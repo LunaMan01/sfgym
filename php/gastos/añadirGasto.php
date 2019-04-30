b06b69ab116768d641b6fccb2e380d171bc902dc
CarlosVega250 <csvega250@gmail.com>
2019-04-30 02:24:25 -0500

    Gastos js

--- php/gastos/añadirGasto.php
+++ php/gastos/añadirGasto.php
@@ -5,10 +5,10 @@
         $añadir = $conn->prepare('INSERT INTO Gastos (descripcion_gasto, monto_gasto, fecha_gasto, Id_Tipo) VALUES 
         (:descripcion, :monto, :fecha, :tipo)');
         
-        $añadir->binParam(':descripcion', $_POST['descripcion-gasto']);
-        $añadir->binParam(':monto', $_POST['monto-gasto']);
-        $añadir->binParam(':fecha', $_POST['fecha-gasto']);
-        $añadir->binParam(':tipo', $_POST['tipo-gasto']);
+        $añadir->bindParam(':descripcion', $_POST['descripcion-gasto']);
+        $añadir->bindParam(':monto', $_POST['monto-gasto']);
+        $añadir->bindParam(':fecha', $_POST['fecha-gasto']);
+        $añadir->bindParam(':tipo', $_POST['tipo-gasto']);
         
         $añadir->execute();
     }catch(PDOException $e){
--- php/gastos/consultarGastos.php
+++ php/gastos/consultarGastos.php
@@ -2,19 +2,20 @@
     include '../conexion.php';
 
     try{
-        $consultar = 'SELECT Id_Gasto, descripcion_gasto, fecha_gasto, monto_gasto 
+        $consultar = 'SELECT Id_Gasto, descripcion_gasto, fecha_gasto, monto_gasto, Id_tipo 
         FROM Gastos';
 
         foreach($conn->query($consultar) as $row){
             echo '<tr>
                 <th scope="row" id="'.$row['Id_Gasto'].'">'.$row['Id_Gasto'].'</th>'.
                 '<td>'.$row['descripcion_gasto'].'</td>'.
-                '<td>'.$row['fecha_gasto'].'</td>'.
                 '<td>'.$row['monto_gasto'].'</td>'.
+                '<td>'.$row['fecha_gasto'].'</td>'.
+                '<td>'.$row['Id_tipo'].'</td>'.
             '<td>
-                <i class="material-icons actions watch-action mr-2" data-toggle="modal" href="#ver-membresia-modal"> remove_red_eye</i>
-                <i class="material-icons actions edit-action mr-2" data-toggle="modal" href="#modificar-membresia-modal"> create</i>
-                <i class="material-icons actions delete-action mr-2" data-toggle="modal" href="#eliminar-membresia-modal"> delete</i> </td>
+                <i class="material-icons actions watch-action mr-2" data-toggle="modal" href="#ver-gasto-modal"> remove_red_eye</i>
+                <i class="material-icons actions edit-action mr-2" data-toggle="modal" href="#modificar-gasto-modal"> create</i>
+                <i class="material-icons actions delete-action mr-2" data-toggle="modal" href="#eliminar-gasto-modal"> delete</i> </td>
             </tr>';
         }
     }catch(PDOException $e){

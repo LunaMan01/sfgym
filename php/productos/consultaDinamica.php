b3c8f236a74b7b05bace9f0102ca49fcd77fdc1f
Luna16290847 <luis-luna98@hotmail.com>
2019-05-10 21:34:53 -0500

    reportesProductos(una parte), modificaciones en otros reportes

--- php/productos/consultaDinamica.php
+++ php/productos/consultaDinamica.php
@@ -4,11 +4,15 @@
     $dato = $_POST['dato'];
 
     try{
-        $query = $conn->prepare('SELECT Id_Producto, descripcion_producto, fecha_caducidad, existencia_producto
+        $query = $conn->prepare('SELECT Id_Producto, descripcion_producto, fecha_caducidad, existencia_producto, precio_producto
         FROM Productos WHERE 
-        Id_Producto LIKE ? OR descripcion_producto LIKE ? OR fecha_caducidad LIKE ? OR existencia_producto LIKE ?');
+        Id_Producto LIKE ? OR 
+        descripcion_producto LIKE ? OR 
+        fecha_caducidad LIKE ? OR 
+        existencia_producto LIKE ? OR
+        precio_producto LIKE ?');
 
-        $query->execute(array($dato."%", $dato."%", $dato."%", $dato."%"));
+        $query->execute(array($dato."%", $dato."%", $dato."%", $dato."%", $dato."%"));
 
         while($results = $query->fetch()){
             echo '<tr>
@@ -16,6 +20,7 @@
             '<td>'.$results['descripcion_producto'].'</td>'.
             '<td>'.$results['fecha_caducidad'].'</td>'.
             '<td>'.$results['existencia_producto'].'</td>'.
+            '<td>'.$results['precio_producto'].'</td>'.
             '<td>
                 <i class="material-icons actions mr-2" watch-action data-toggle="modal" href="#ver-producto-modal"> remove_red_eye</i>
                 <i class="material-icons actions edit-action mr-2" data-toggle="modal" href="#modificar-producto-modal"> create</i>

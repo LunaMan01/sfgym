df3d31a0b43c223c39bb55f455b1aa5b697ed9b6
CarlosVega250 <csvega250@gmail.com>
2019-05-15 23:18:32 -0500

    Mandar opcion select

--- js/Models/Cliente.js
+++ js/Models/Cliente.js
@@ -30,11 +30,11 @@ class Cliente {
         return true;
     }
 
-    consultar(datoABuscar) {
+    consultar(datoABuscar, selectActual) {
         var req = new XMLHttpRequest();
         req.open("POST", 'php/clientes/consultaDinamica.php', false);
         req.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded; charset=UTF-8');
-        req.send('dato=' + datoABuscar);
+        req.send('dato=' + datoABuscar+"&select-clientes="+selectActual);
         return req.responseText;
     }
 

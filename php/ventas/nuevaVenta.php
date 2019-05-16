ee9b02a598eecb481ae7437285484291f497a4f9
Luna16290847 <luis-luna98@hotmail.com>
2019-05-16 18:49:36 -0500

    agregarVenta; DB

--- php/ventas/nuevaVenta.php
+++ php/ventas/nuevaVenta.php
@@ -5,31 +5,41 @@
 
     try{
         $sell = $_POST['venta'];
-        $arraySell = json_decode($json);
+        $arraySell = json_decode($sell, true);
+        print_r($arraySell);
 
         $items = $_POST['productos'];
-        $arrayItems = json_decode($json);
+        $arrayItems = json_decode($items, true);
+        print_r($arrayItems);
 
-        foreach($arraySell as $row){
-
-        }
+        agregarVentas($conn, $arraySell['nipCliente'], $arraySell['idInstructor'], $arraySell['totalVenta']);
 
         $lastId = $conn->lastInsertId();
+        echo $lastId;
 
         foreach($arrayItems as $row){
             detalleVenta($conn, $lastId, $row['id'], $row['cantidad'], $row['subtotal']);
         }
 
-        print_r($array);
-
     }catch(PDOException $e){
         echo 'Error: '. $e->getMessage();
+    }
+
+    function agregarVentas($conn, $cliente, $instructor, $total){
+        $venta = $conn->prepare("INSERT INTO Ventas (Id_Cliente, Id_Instructor, total_venta)
+        VALUES (:idCliente, :idInstructor, :total)");
+
+        $venta->bindParam(':idCliente', $cliente);
+        $venta->bindParam(':idInstructor', $instructor);
+        $venta->bindParam(':total', $total);
+
+        $venta->execute();
     }
 
     function detalleVenta($conn, $venta, $producto, $cantidad, $total){
         global $fecha;
         $detalle = $conn->prepare("INSERT INTO VentasProductos (Id_Venta, Id_Producto, cantidad_producto, total_venta, fecha_venta)
-        VALUES :idVenta, :idProducto, :cantidad, :total, :fecha");
+        VALUES (:idVenta, :idProducto, :cantidad, :total, :fecha)");
 
         $detalle->bindParam(':idVenta', $venta);
         $detalle->bindParam(':idProducto', $producto);
@@ -38,10 +48,6 @@
         $detalle->bindParam(':fecha', $fecha);
 
         $detalle->execute();
-    }
-
-    function agregarVentas(){
-
     }
     $conn == null;
 ?>

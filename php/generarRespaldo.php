17da682bc70a3756890e6ba1dfb05fd0d73fbbf2
CarlosVega250 <csvega250@gmail.com>
2019-05-27 23:16:30 -0500

    Respaldos sin mysqldump

--- php/generarRespaldo.php
+++ php/generarRespaldo.php
@@ -17,8 +17,17 @@
      }
      mysqli_free_result($result);
 
-     $return = '';
-     foreach ($tables as $table) {
+     $return = "/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+     /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+     /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+     /*!40101 SET NAMES utf8 */;
+     /*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+     /*!40103 SET TIME_ZONE='+00:00' */;
+     /*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+     /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+     /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+     /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;\n\n\n";
+    foreach ($tables as $table) {
 
          $result = mysqli_query($connection, "SELECT * FROM " . $table);
          if (!$result) {
@@ -28,18 +37,21 @@
              if (!$num_fields) {
                  $backupAlert = 'Error found.<br/>ERROR : ' . mysqli_error($connection) . 'ERROR NO :' . mysqli_errno($connection);
              } else {
-                 $return .= 'DROP TABLE IF EXISTS `' . $table . '`;';
+                 $return .= "\n\nDROP TABLE IF EXISTS `" . $table . "`;\n";
                  $return .= '/*!40101 SET @saved_cs_client = @@character_set_client */;
                  /*!40101 SET character_set_client = utf8 */;';
                 
 
                  $row2 = mysqli_fetch_row(mysqli_query($connection, 'SHOW CREATE TABLE ' . $table));
+                 
                  if (!$row2) {
                      $backupAlert = 'Error found.<br/>ERROR : ' . mysqli_error($connection) . 'ERROR NO :' . mysqli_errno($connection);
                  } else {
+                    
                      $return .= "\n\n" . $row2[1] . ";\n\n";
-                     $return .= 'LOCK TABLES `' .$table. "` WRITE;;
-                     /*!40000 ALTER TABLE `tipogastos` DISABLE KEYS */;";
+                     $return .= "\n/*!40101 SET character_set_client = @saved_cs_client */;";
+                     $return .= 'LOCK TABLES `' .$table. "` WRITE;\n
+                     /*!40000 ALTER TABLE `".$table."` DISABLE KEYS */;\n";
                      for ($i = 0; $i < $num_fields; $i++) {
                          while ($row = mysqli_fetch_row($result)) {
                              $return .= 'INSERT INTO ' . $table . ' VALUES(';
@@ -59,18 +71,30 @@
                      }
                      $return .= "\n\n\n";
                      $return .= "/*!40000 ALTER TABLE `".$table."` ENABLE KEYS */;\nUNLOCK TABLES;";
-                 }
 
+                     
+                 }
+                 
                  $backup_file = $dbname . date("Y-m-d-H-i-s") . '.sql';
                  $handle = fopen("{$backup_file}", 'w+');
                  fwrite($handle, $return);
                  fclose($handle);
-                 $backupAlert = 'Succesfully got the backup!';
+                 
              }
+           
+
          }
-     }
+       
+    }
+   
+    header('Content-Description: File Transfer');
+    header('Content-Type: application/sql');
+    header('Content-Disposition: attachment; filename="'.basename($backup_file).'"');
+    readfile("$backup_file");
+
+    unlink($backup_file);
  }
- echo $backupAlert;
+
 
 
 

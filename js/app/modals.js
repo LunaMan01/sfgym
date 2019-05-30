fd481fd07446401ecff2636b51fb2c554e2e8fab
OctavioBautista <Octavio16290837@itcg.edu.mx>
2019-05-30 01:36:51 -0500

    correción

--- js/app/modals.js
+++ js/app/modals.js
@@ -875,7 +875,7 @@ aria-hidden="true">
 <div class="modal-dialog" role="document">
     <div class="modal-content">
         <div class="modal-header">
-            <h5 class="modal-title">Modificar compra</h5>
+            <h5 class="modal-title">Compra</h5>
             <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                 <span aria-hidden="true">&times;</span>
             </button>

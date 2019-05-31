75edafaaa8cc9e0773851c36e3e081589d3fdc55
CarlosVega250 <csvega250@gmail.com>
2019-05-31 09:44:12 -0500

    Alineciones

--- js/Views/UIVenta.js
+++ js/Views/UIVenta.js
@@ -65,9 +65,9 @@ var UIVenta = (function () {
                     <th scope="row" class="carrito" id="${productoId}" data-cantidad="${cantidad}" data-subtotal = "${subtotal}"
                     >${productoId}</th>
                     <td>${producto}</td>
-                    <td data-precio = "${precio}">${cantidad}</td>
-                    <td class="subtotales">${subtotal}</td>
-                    <td>
+                    <td class="text-right" data-precio = "${precio}">${cantidad}</td>
+                    <td class="subtotales text-right">${subtotal}</td>
+                    <td class="text-right">
                 <i class="material-icons actions edit-action mr-2" data-toggle="modal" href="#modificar-cantidad-producto-modal"> create</i>
                 <i class="material-icons actions delete-action mr-2" data-toggle="modal" href="#eliminar-membresia-modal"> delete</i> </td>
                 </tr>

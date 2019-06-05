a1605f960010ddb57994d66e5356e3949bf9417f
Luna16290847 <luis-luna98@hotmail.com>
2019-06-04 23:16:28 -0500

    agregarCompras

--- php/compras/añadirCompras.php
+++ php/compras/añadirCompras.php
@@ -7,47 +7,70 @@
         $buy = $_POST['compra'];
         $arrayBuy = json_decode($buy, true);
 
-        $items = $_POST['productos'];
-        $arrayItems = json_decode($items, true);
+        $items1 = $_POST['productos-nuevos'];
+        $arrayItems1 = json_decode($items1, true);
+
+        $items2 = $_POST['productos-existentes'];
+        $arrayItems2 = json_decode($items2, true);
 
         $idIntructor = $arrayBuy['idInstructor'];
         $tipoCompra = $arrayBuy['tipoCompra'];
-        $descripcion = $arrayBuy['descripcionCompra'];
+        $categoria = $arrayBuy['categoria'];
+        $cantidad = $arrayBuy['cantidad'];
+        $aparato = $arrayBuy['aparato'];
         $totalCompra = $arrayBuy['totalCompra'];
 
-        agregarCompra($conn, $idIntructor, $tipoCompra, $descripcion, $totalCompra);
+        agregarCompra($conn, $idIntructor, $tipoCompra, $categoria, $descripcion, $totalCompra);
 
+        $lastIdCompra = $conn->lastInsertId();
 
-        // if($_POST['compras'] == 1){
-        //     agregarProducto($conn);
-        //     echo 1;
-        // }
+        //TIPO DE COMPRA
+        //PRODUCTOS
+        if($_POST['select-tipo-compra'] == 1){
+            
+            //CATEGORIA
+            //PRODUCTOS NUEVOS
+            if($_POST['compras'] == 1){
+                foreach($arrayItems1 as $row){
+                    comprasProductosNuevos($conn, $lastIdCompra, $row['descripcion'], $row['fecha-caducidad'], $row['existencia'], $row['precio'], $row['subtotal']);
+                }
+                echo 1;
+            }
 
-        // if($_POST['compras'] == 2){
-        //     agregarAparato($conn);
-        //     echo 1;
-        // }
+            //PRODUCTOS EXISTENTES
+            if($_POST['compras'] == 2){
+                foreach($arrayItems2 as $row){
+                    comprasProductosExistentes($conn, $lastIdCompra, $row['id'], $row['existencia'], $row['subtotal']);
+                }
+                echo 1;
+            }
+        }
 
-        // if($_POST['compras'] == 3){
-        //     agregarCompra($conn);
-        //     echo 1;
-        // }
+        //APARATOS
+        if($_POST['select-tipo-compra'] == 2){
+            foreach($arrayItems as $row){
+                comprasAparatos($conn, $lastIdCompra, $row['descripcion'], $row['subtotal']);
+            }
+            echo 1;
+        }
+
     }catch(PDOException $e){
         echo 'Error: '. $e->getMessage();
     }
 
-    function agregarCompra($conn, $idIntructor, $tipoCompra, $descripcion, $totalCompra){
+    function agregarCompra($conn, $idIntructor, $tipoCompra, $categoria, $descripcion, $totalCompra){
         global $fechaActual;
-
-        $agregar = $conn->prepare('INSERT INTO Compras (Id_Instructor, Id_TipoCompra, descripcion_compra, monto_compra, fecha_compra) VALUES 
-            (:instructor, :tipo, :descripcion, :monto, :fecha)');
+        try{
+        $agregar = $conn->prepare('INSERT INTO Compras (Id_Instructor, Id_TipoCompra, Id_Categoria, descripcion_compra, total_compra, fecha_compra, cancelada) VALUES 
+            (:instructor, :tipo, :categoria, :descripcion, :total, :fecha, 0)');
 
         $agregar->bindParam(':instructor', $idIntructor);
         $agregar->bindParam(':tipo', $tipoCompra);
+        $agregar->bindParam(':categoria', $categoria);
         $agregar->bindParam(':descripcion', $descripcion);
-        $agregar->bindParam(':monto', $totalCompra);
+        $agregar->bindParam(':total', $totalCompra);
         $agregar->bindParam(':fecha', $fechaActual);
-        try{
+        
         $agregar->execute();
         
         }catch(PDOException $e){
@@ -55,53 +78,71 @@
         }
     }
 
-    // function actualizarProducto($conn){
-    //     agregarCompra($conn);
+    function comprasProductosNuevos($conn, $lastIdCompra, $descripcion, 
+        $fechaCaducidad, $existenciaProducto, $precioProducto, $subtotal){
+        agregarCompra($conn);
 
-    //     $compra = $conn->prepare('UPDATE Productos set existencia_producto = :existencia WHERE Id_Producto = :idP');
+        $producto = $conn->prepare('INSERT INTO Productos (descripcion_producto, fecha_caducidad, existencia_producto, precio_producto)
+            VALUES (:nombre, :fecha, :existencia, :precio)');
+    
+        $producto->bindParam(':nombre', $descripcion);
+        $producto->bindParam(':fecha', $fechaCaducidad);
+        $producto->bindParam(':existencia', $existenciaProducto);
+        $producto->bindParam(':precio', $precioProducto);
 
-    //     $compra->bindParam(':existencia', $_POST['cantidad']);
-    //     $compra->bindParam(':idP', $_POST['id']);
-       
+        $producto->execute();
 
-    //     $compra->execute();
-    // }
+        $lastIdProducto = $conn->lastInsertId();
 
-    // function agregarProducto($conn){
-    //     agregarCompra($conn);
+        $compra = $conn->prepare('INSERT INTO ComprasProductos (Id_Compra, Id_Producto, total)
+            VALUES (:compra, :producto, :subtotal)');
+        
+        $compra->bindParam(':compra', $lastIdCompra);
+        $compra->bindParam(':producto', $lastIdProducto);
+        $compra->bindParam(':subtotal', $subtotal);
 
-    //     $compra = $conn->prepare('INSERT INTO Productos (descripcion_producto, fecha_caducidad, existencia_producto, precio_producto)
-    //         VALUES (:nombre, :fecha, :existencia, :precio)');
+        $compra->execute();
+    }
 
-    //     $compra->bindParam(':nombre', $_POST['descripcion-compra']);
-    //     $compra->bindParam(':fecha', $_POST['fecha-caducidad']);
-    //     $compra->bindParam(':existencia', $_POST['existencia']);
-    //     $compra->bindParam(':precio', $_POST['precio-producto']);
+    function comprasProductosExistentes($conn, $lastIdCompra, $idProducto, $existencia, $subtotal){
+        agregarCompra($conn);
 
-    //     $compra->execute();
+        $compra = $conn->prepare('UPDATE Productos SET 
+            existencia_producto = :existencia
+            WHERE Id_Producto = '. $idProducto);
 
-    //     $productos = $conn->prepare('INSERT INTO ComprasProductos (Id_Compra, Id_Producto, existencia_producto, precio_producto)
-    //         VALUES (:nombre, :fecha, :existencia, :precio)');
+        $compra->bindParam(':existencia', $existencia);
 
-    //     $productos->bindParam(':nombre', $_POST['descripcion-compra']);
-    //     $productos->bindParam(':fecha', $_POST['fecha-caducidad']);
-    //     $productos->bindParam(':existencia', $_POST['existencia']);
-    //     $productos->bindParam(':precio', $_POST['precio-producto']);
+        $compra->execute();
 
-    //     $productos->execute();
+        $productos = $conn->prepare('INSERT INTO ComprasProductos (Id_Compra, Id_Producto, total)
+            VALUES (:compra, :producto, :subtotal)');
 
+        $productos->bindParam(':compra', $lastIdCompra);
+        $productos->bindParam(':producto', $idProducto);
+        $productos->bindParam(':subtotal', $subtotal);
 
-    // }
+        $productos->execute();
+    }
 
-    // function agregarAparato($conn){
-    //     agregarCompra($conn);
+    function comprasAparatos($conn, $lastIdCompra, $descripcion){
+        agregarCompra($conn);
 
-    //     $aparato = $conn->prepare('INSERT INTO Aparatos (nombre_aparato) 
-    //         VALUES (:aparato)');
+        $aparato = $conn->prepare('INSERT INTO Aparatos (nombre_aparato) 
+            VALUES (:aparato)');
 
-    //     $aparato->bindParam(':aparato', $_POST['descripcion-compra']);
+        $aparato->bindParam(':aparato', $descripcion);
 
-    //     $aparato->execute();
-    // }
+        $aparato->execute();
+
+        $lastIdAparato = $conn->lastInsertId();
+
+        $compra = $conn->prepare('INSERT INTO ComprasAparatos (Id_Compra, Id_Aparato, total)
+            VALUES (:compra, :aparato, :total)');
+
+        $compras->bindParam(':compra', $lastIdCompra);
+        $compras->bindParam(':aparato', $lastIdAparato);
+        $compras->bindParam(':total', $descripcion);
+    }
     $conn = null;
 ?>
